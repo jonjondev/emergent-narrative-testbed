67e1ef8933eaec683b1814511f28085ea1f3213e
Jonathan Moallem <jdmoallem@gmail.com>
2020-06-24 16:04:36 +1000

    Imported 3DText scene, scripts, and assets into project

---
+++ src/scripts/3DText.gd
@@ -0,0 +1,18 @@
+tool
+extends Spatial
+
+export (String) var text: String setget set_text
+export (bool) var is_billboard: bool
+
+func _process(_delta):
+	if is_billboard:
+		var camera: Camera = get_viewport().get_camera()
+		var look: Vector3 = camera.to_global(Vector3(0, 0, -100)) - camera.global_transform.origin
+		look_at(translation + look, Vector3.UP)
+		rotate_object_local(Vector3.BACK, camera.rotation.z)
+
+func set_text(value):
+	text = value
+	if not is_inside_tree():
+		yield(self, 'ready')
+	$Viewport/Control/Label.text = text
