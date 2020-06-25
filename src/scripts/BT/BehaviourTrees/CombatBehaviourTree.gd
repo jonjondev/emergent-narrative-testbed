64a73fc3d34fd5734369980dc94894eb971c844f
Jonathan Moallem <jdmoallem@gmail.com>
2020-06-25 16:20:56 +1000

    Removed unused actions and behaviour trees from imported behaviour tree module

--- src/scripts/BT/BehaviourTrees/CombatBehaviourTree.gd
+++
@@ -1,9 +0,0 @@
-class_name CombatBehaviourTree
-extends BehaviourTree
-
-func _init(o).(o):
-	root = \
-	Sequence.new([
-		NavigateToBehaviourTree.new(owner, "enemy"),
-		AttackAction.new(owner, "enemy"),
-	])
--- src/scripts/BT/BehaviourTrees/NavigateToBehaviourTree.gd
+++
@@ -1,9 +0,0 @@
-class_name NavigateToBehaviourTree
-extends BehaviourTree
-
-func _init(o, target_id).(o):
-	root = \
-	ActiveSelector.new([
-		NearCondition.new(owner, target_id),
-		NavigateAction.new(owner, target_id),
-	])
