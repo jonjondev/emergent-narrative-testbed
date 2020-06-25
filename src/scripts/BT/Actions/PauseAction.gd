1b53c1b3418b383d2c34d1031c71a8432371ad5e
Jonathan Moallem <jdmoallem@gmail.com>
2020-06-25 16:21:23 +1000

    Created pause action and added to simple behaviour tree

---
+++ src/scripts/BT/Actions/PauseAction.gd
@@ -0,0 +1,24 @@
+class_name PauseAction
+extends Action
+
+var pause_time
+var is_complete
+
+func _init(o, time).(o):
+	pause_time = time
+
+func on_initialise() -> void:
+	owner.get_tree().create_timer(pause_time).connect("timeout", self, "complete")
+	.on_initialise()
+
+func update() -> int:
+	if is_complete:
+		return Status.SUCCESS
+	return Status.RUNNING
+
+func on_terminate(status) -> void:
+	is_complete = false
+	.on_terminate(status)
+
+func complete() -> void:
+	is_complete = true
--- src/scripts/BT/BehaviourTrees/SimpleBehaviourTree.gd
+++ src/scripts/BT/BehaviourTrees/SimpleBehaviourTree.gd
@@ -3,19 +3,13 @@ extends BehaviourTree
 
 func _init(o).(o):
 	root = \
-	Selector.new([
-		Sequence.new([
-			GetVarAction.new(owner, "eaten", false),
-			VisibleCondition.new(owner, "food"),
-			EmoteAction.new(owner, "*hungry*"),
-			NavigateAction.new(owner, "food"),
-			EmoteAction.new(owner, "*eating*"),
-			SetVarAction.new(owner, "eaten", true),
-		]),
-		Sequence.new([
-			EmoteAction.new(owner, "*sleepy*"),
-			NavigateAction.new(owner, "bed"),
-			EmoteAction.new(owner, "*sleeping*"),
-			SetVarAction.new(owner, "eaten", false),
-		]),
+	Sequence.new([
+		EmoteAction.new(owner, "*hungry*"),
+		NavigateAction.new(owner, "food"),
+		EmoteAction.new(owner, "*eating*"),
+		PauseAction.new(owner, 1.0),
+		EmoteAction.new(owner, "*sleepy*"),
+		NavigateAction.new(owner, "bed"),
+		EmoteAction.new(owner, "*sleeping*"),
+		PauseAction.new(owner, 1.0),
 	])
