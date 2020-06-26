cb7980ba21403f3919df1c950af0938529e0af74
Jonathan Moallem <jdmoallem@gmail.com>
2020-06-26 15:37:02 +1000

    Fixed 3DText editor errors

--- src/scripts/3DText.gd
+++ src/scripts/3DText.gd
@@ -3,10 +3,11 @@ extends Spatial
 
 export (String) var text: String setget set_text
 export (bool) var is_billboard: bool
+var camera : Camera
 
 func _process(_delta):
-	if is_billboard:
-		var camera: Camera = get_viewport().get_camera()
+	camera = get_viewport().get_camera()
+	if is_billboard and camera:
 		var look: Vector3 = camera.to_global(Vector3(0, 0, -100)) - camera.global_transform.origin
 		look_at(translation + look, Vector3.UP)
 		rotate_object_local(Vector3.BACK, camera.rotation.z)
