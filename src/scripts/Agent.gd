04e175a6093db709ce0a2157ffccb91ccc44506e
Jonathan Moallem <jdmoallem@gmail.com>
2020-08-20 16:41:46 +1000

    Moved agent perception out of navigation behaviour

--- src/scripts/Agent.gd
+++ src/scripts/Agent.gd
@@ -15,6 +15,7 @@ onready var anim_state_machine: AnimationNodeStateMachinePlayback = $Model/Anima
 
 func _ready():
 	var _err = $AITime.connect("timeout", self , "ai_process")
+	_err = perception.connect("area_entered", self, "percieve")
 
 func _physics_process(delta) -> void:
 	navigation.process_navigation(delta)
@@ -31,3 +32,11 @@ func ai_process() -> void:
 
 func emote(text: String) -> void:
 	emote_text.text = text + "\n" + character_name
+
+func percieve(area) -> void:
+	if area is SmartObject and area.self_value != self:
+		for group in area.get_groups():
+			if blackboard.get(group):
+				blackboard[group].append(area)
+			else:
+				blackboard[group] = [area]
--- src/scripts/BT/Actions/NavigateAction.gd
+++ src/scripts/BT/Actions/NavigateAction.gd
@@ -10,9 +10,7 @@ func _init(o, group, distance = 1.0).(o):
 	min_distance = distance
 
 func on_initialise() -> void:
-	target = owner.blackboard.get(group_name)
-	if target and not owner.navigation.is_near(target.self_value.translation, min_distance):
-		owner.navigation.navigate_to(target.self_value.translation)
+	check_memory()
 	.on_initialise()
 
 func update() -> int:
@@ -22,12 +20,15 @@ func update() -> int:
 		else:
 			owner.navigation.move_to_random_location()
 	else:
-		for potential_target in owner.perception.get_overlapping_areas():
-			if potential_target.is_in_group(group_name) and potential_target.self_value != owner:
-				target = potential_target
-				owner.blackboard[group_name] = target
-				owner.navigation.navigate_to(target.self_value.translation)
+		check_memory()
 	return Status.RUNNING
 
 func on_terminate(status) -> void:
 	.on_terminate(status)
+
+func check_memory() -> void:
+	var potential_targets = owner.blackboard.get(group_name)
+	if potential_targets:
+		target = owner.navigation.get_closest(potential_targets)
+		if not owner.navigation.is_near(target.self_value.translation, min_distance):
+			owner.navigation.navigate_to(target.self_value.translation)
