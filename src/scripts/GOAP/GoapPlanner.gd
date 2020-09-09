592ca6514dbe20e530527b8f6447ccfd138e028d
Jonathan Moallem <jdmoallem@gmail.com>
2020-09-09 17:38:59 +1000

    Refactored GOAP planner to support multiple plan generation

--- src/scripts/GOAP/GoapPlanner.gd
+++ src/scripts/GOAP/GoapPlanner.gd
@@ -1,28 +1,33 @@
 class_name GoapPlanner
 
 static func generate_plan(current_state, profile):
-	return generate_valid_paths(current_state, profile) if not conditions_valid(current_state, profile.goal_state) else null
+	if conditions_valid(current_state, profile.goal_state):
+		return null
+	var potential_plans = generate_valid_paths(current_state, profile)
+	var selected_plan_idx = 0
+	while contains_indirection(potential_plans[selected_plan_idx]) and selected_plan_idx < potential_plans.size() - 1:
+		selected_plan_idx += 1
+	return potential_plans[selected_plan_idx]
 
 static func generate_valid_paths(current_state, profile):
 	var found_plans = []
 	var checked_actions = []
-	var potential_state_paths = [[current_state, []]]
+	var potential_state_paths = [new_state_path(current_state, [])]
 	while not potential_state_paths.empty():
 		var state_path = potential_state_paths.pop_front()
 		for action in profile.actions:
 			if checked_actions.has(action):
 				continue
-			elif conditions_valid(state_path[0], action.preconditions):
-				var new_actions = state_path[1].duplicate()
+			if conditions_valid(state_path.conditions, action.preconditions):
+				var new_actions = state_path.actions.duplicate()
 				new_actions.append(action)
-				var action_state = apply_effects(state_path[0], action.effects)
-				checked_actions.append(action)
+				var action_state = apply_effects(state_path.conditions, action.effects)
 				if conditions_valid(action_state, profile.goal_state):
 					found_plans.append(new_actions)
-					#return new_actions
 				else:
-					potential_state_paths.append([action_state, new_actions])
-	return found_plans[0]
+					potential_state_paths.append(new_state_path(action_state, new_actions))
+					checked_actions.append(action)
+	return found_plans
 
 static func conditions_valid(state, conditions):
 	for condition in conditions.keys():
@@ -35,3 +40,21 @@ static func apply_effects(state, effects):
 	for effect in effects.keys():
 		new_state[effect] = effects[effect]
 	return new_state
+
+static func contains_indirection(plan):
+	for action in plan:
+		if action.is_indirect:
+			return true
+	return false
+
+static func new_state_path(conditions, actions):
+	return { "conditions": conditions, "actions": actions}
+
+static func get_script_name(action):
+	return action.get_script().resource_path.split("/")[-1].split(".")[0]
+
+static func print_plan(plan):
+	var p = "["
+	for action in plan:
+		p += (get_script_name(action) + ", ")
+	print(p.rstrip(", ") + "]")
