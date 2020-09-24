e3c5001ff1d939de8af2d106782a122b64aff527
Jonathan Moallem <jdmoallem@gmail.com>
2020-09-24 12:07:21 +1000

    Simplified drama manager agent

--- src/scripts/DramaManager/DMAgent.gd
+++ src/scripts/DramaManager/DMAgent.gd
@@ -15,8 +15,8 @@ func generate_current_state():
 	var agent = owner.get_tree().get_nodes_in_group("agent")[0]
 	current_state[HomeStates.StateConditions.IS_HUNGRY] = agent.blackboard.get("hunger") <= 0
 	current_state[HomeStates.StateConditions.IS_SLEEPY] = agent.blackboard.get("energy") <= 0
-	current_state[HomeStates.StateConditions.DOOR_UNCHECKED] = any_doors_knocked(agent)
-	current_state[HomeStates.StateConditions.DOOR_OPENED] = any_doors_opened(agent)
+	current_state[HomeStates.StateConditions.DOOR_UNCHECKED] = get_agent_profile(agent).any_doors_knocked()
+	current_state[HomeStates.StateConditions.DOOR_OPENED] = get_agent_profile(agent).any_doors_opened()
 	current_state[HomeStates.StateConditions.IS_BORED] = not current_state[HomeStates.StateConditions.IS_SLEEPY] and not current_state[HomeStates.StateConditions.IS_HUNGRY]
 	current_state[HomeStates.StateConditions.KNOWS_FOOD] = check_agent_memory(agent, "food")
 	current_state[HomeStates.StateConditions.KNOWS_BED] = check_agent_memory(agent, "bed")
@@ -24,19 +24,8 @@ func generate_current_state():
 	current_state[HomeStates.StateConditions.KNOWS_DOOR] = check_agent_memory(agent, "door")
 	return current_state
 
+func get_agent_profile(agent):
+	return agent.behaviour_algorithm.agent_profile
+
 func check_agent_memory(agent, group_name):
 	return true if agent.blackboard.get(group_name) else false
-
-func any_doors_knocked(agent) -> bool:
-	if agent.blackboard.get("door"):
-		for door in agent.blackboard.get("door"):
-			if door.knocked:
-				return true
-	return false
-
-func any_doors_opened(agent) -> bool:
-	if agent.blackboard.get("door"):
-		for door in agent.blackboard.get("door"):
-			if door.opened:
-				return true
-	return false
