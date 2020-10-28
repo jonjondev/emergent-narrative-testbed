d3394b32f7bb26d8b5c9ed7dc1e33ec9ca75787f
Jonathan Moallem <jdmoallem@gmail.com>
2020-10-28 16:48:31 +1100

    Added new GOAP actions for picking up the gun, running away and starting fires

---
+++ src/scripts/GOAP/actions/PickupGunAction.gd
@@ -0,0 +1,36 @@
+class_name PickupGunAction
+extends GoapAction
+
+var is_initialised = false
+var is_complete = false
+
+func _init(o).(o):
+	preconditions = {
+		HomeStates.StateConditions.KNOWS_GUN: true,
+	}
+	effects = {
+		HomeStates.StateConditions.HAS_GUN: true,
+	}
+
+func setup() -> void:
+	target = owner.get_closest("gun")
+	owner.emote("*getting gun*")
+
+func perform():
+	if not is_initialised and owner.navigation.is_near(target.translation, 1.5):
+		var interaction_data = target.interact(owner)
+		owner.emote(interaction_data.name)
+		owner.get_tree().create_timer(interaction_data.length).connect("timeout", self, "complete")
+		is_initialised = true
+	elif is_complete:
+		reset()
+		return true
+	return false
+
+func complete() -> void:
+	is_complete = true
+
+func reset() -> void:
+	owner.emote("")
+	is_complete = false
+	is_initialised = false
---
+++ src/scripts/GOAP/actions/StartFireAction.gd
@@ -0,0 +1,12 @@
+class_name StartFireAction
+extends GoapAction
+
+func _init(o).(o):
+	preconditions = {}
+	effects = {
+		HomeStates.StateConditions.IS_SAFE: false,
+	}
+
+func perform():
+	owner.get_tree().get_nodes_in_group("fire")[0].start_fire()
+	return true
