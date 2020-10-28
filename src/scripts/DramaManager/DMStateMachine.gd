bc42412ab9362af5ae8c4a5753bbc630a1ca8355
Jonathan Moallem <jdmoallem@gmail.com>
2020-10-28 19:29:42 +1100

    Simplified drama manager system

--- src/scripts/DramaManager/DMStateMachine.gd
+++ src/scripts/DramaManager/DMStateMachine.gd
@@ -2,8 +2,6 @@ class_name DMStateMachine
 
 enum State { PLANNING, ACTION }
 
-var agent_profiles: Array = []
-
 # State machine fields
 var owner: Node
 var current_state: int = State.PLANNING
@@ -39,14 +37,15 @@ func on_update():
 				on_action_update()
 
 func on_planning_update():
+	print(dm_profile.goal_state.size())
+	if dm_profile.goal_state.empty() and not dm_profile.goal_states.empty():
+		var new_key = dm_profile.goal_states.keys()[0]
+		var new_value = dm_profile.goal_states[new_key]
+		dm_profile.goal_state[new_key] = new_value
+		dm_profile.goal_states.erase(new_key)
 	var agents = owner.get_tree().get_nodes_in_group("agent")
-	var potential_actions = []
-	for agent in agents:
-		var new_action = generate_plan(dm_profile.states.generate_current_state(agent))
-		if new_action:
-			potential_actions.append(new_action)
-	if potential_actions.size() > 0 and potential_actions[0] != current_action:
-		current_action = potential_actions[0]
+	if agents.size() > 0:
+		current_action = generate_dm_plan(dm_profile.states.generate_current_state(agents[0]), agents)
 
 func on_action_update():
 	if current_action && GoapPlanner.conditions_valid(dm_profile.states.generate_current_state(owner.get_tree().get_nodes_in_group("agent")[0]), current_action.preconditions):
@@ -59,10 +58,12 @@ func on_action_update():
 	else:
 		current_action = null
 
-func generate_plan(initial_state):
+func generate_dm_plan(initial_state, agents):
 	if GoapPlanner.conditions_valid(initial_state, dm_profile.goal_state):
+		dm_profile.goal_state.clear()
 		return null
-	for agent_profile in agent_profiles:
+	for agent in agents:
+		var agent_profile = agent.behaviour_algorithm.agent_profile
 		if not state_meets_goals(initial_state, agent_profile, dm_profile.goal_state):
 			for action in dm_profile.actions:
 				var new_state = GoapPlanner.apply_effects(initial_state, action.effects)
