398a5a05a97979ab50a3b934c39dae073d0c78f5
Jonathan Moallem <jdmoallem@gmail.com>
2020-11-02 01:51:43 +1100

    Moved agent profiling functions in home states down to base GOAP states class

--- src/scripts/GOAP/states/GoapStates.gd
+++ src/scripts/GOAP/states/GoapStates.gd
@@ -1,10 +1,17 @@
 class_name GoapStates
 
-func generate_base_state(num_states) -> Array:
+static func generate_base_state(num_states) -> Array:
 	var current_state = []
 	for _i in range(num_states):
 		current_state.append(null)
 	return current_state
 
-func check_agent_memory(owner, group_name):
+static func check_agent_memory(owner, group_name) -> bool:
 	return true if owner.blackboard.get(group_name) else false
+
+static func any_of_group(owner, group, field) -> bool:
+	if owner.blackboard.get(group):
+		for object in owner.blackboard.get(group):
+			if object.get(field):
+				return true
+	return false
--- src/scripts/GOAP/states/HomeStates.gd
+++ src/scripts/GOAP/states/HomeStates.gd
@@ -16,7 +16,7 @@ enum StateConditions {
 }
 
 static func generate_current_state(owner):
-	var current_state = .generate_base_state(StateConditions.size())
+	var current_state = generate_base_state(StateConditions.size())
 	current_state[StateConditions.IS_HUNGRY] = owner.blackboard.get("hunger") <= 0
 	current_state[StateConditions.IS_SLEEPY] = owner.blackboard.get("energy") <= 0
 	current_state[StateConditions.IS_SAFE] = not (any_of_group(owner, "fire", "lit") or any_of_group(owner, "intruder", "present"))
@@ -24,15 +24,8 @@ static func generate_current_state(owner):
 	current_state[StateConditions.HAS_GUN] = owner.blackboard.get("has_gun") == true
 	current_state[StateConditions.DOOR_KNOCKED] = any_of_group(owner, "door", "knocked")
 	current_state[StateConditions.DOOR_OPENED] = any_of_group(owner, "door", "opened")
-	current_state[StateConditions.KNOWS_FOOD] = .check_agent_memory(owner, "food")
-	current_state[StateConditions.KNOWS_BED] = .check_agent_memory(owner, "bed")
-	current_state[StateConditions.KNOWS_DOOR] = .check_agent_memory(owner, "door")
-	current_state[StateConditions.KNOWS_GUN] = .check_agent_memory(owner, "gun")
+	current_state[StateConditions.KNOWS_FOOD] = check_agent_memory(owner, "food")
+	current_state[StateConditions.KNOWS_BED] = check_agent_memory(owner, "bed")
+	current_state[StateConditions.KNOWS_DOOR] = check_agent_memory(owner, "door")
+	current_state[StateConditions.KNOWS_GUN] = check_agent_memory(owner, "gun")
 	return current_state
-
-static func any_of_group(owner, group, field) -> bool:
-	if owner.blackboard.get(group):
-		for object in owner.blackboard.get(group):
-			if object.get(field):
-				return true
-	return false
