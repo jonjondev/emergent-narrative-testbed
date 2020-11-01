9bbae9710fdb60efb47ee8c931e4e685f629c2ce
Jonathan Moallem <jdmoallem@gmail.com>
2020-11-01 18:00:31 +1100

    Simplified state generation helper functions for home states

--- src/scripts/GOAP/states/HomeStates.gd
+++ src/scripts/GOAP/states/HomeStates.gd
@@ -19,41 +19,20 @@ static func generate_current_state(owner):
 	var current_state = .generate_base_state(StateConditions.size())
 	current_state[StateConditions.IS_HUNGRY] = owner.blackboard.get("hunger") <= 0
 	current_state[StateConditions.IS_SLEEPY] = owner.blackboard.get("energy") <= 0
-	current_state[StateConditions.IS_SAFE] = not (any_fires(owner) or any_intruders(owner))
+	current_state[StateConditions.IS_SAFE] = not (any_of_group(owner, "fire", "lit") or any_of_group(owner, "intruder", "present"))
 	current_state[StateConditions.IS_HOME] = owner != null
 	current_state[StateConditions.HAS_GUN] = owner.blackboard.get("has_gun") == true
-	current_state[StateConditions.DOOR_KNOCKED] = any_doors_knocked(owner)
-	current_state[StateConditions.DOOR_OPENED] = any_doors_opened(owner)
+	current_state[StateConditions.DOOR_KNOCKED] = any_of_group(owner, "door", "knocked")
+	current_state[StateConditions.DOOR_OPENED] = any_of_group(owner, "door", "opened")
 	current_state[StateConditions.KNOWS_FOOD] = .check_agent_memory(owner, "food")
 	current_state[StateConditions.KNOWS_BED] = .check_agent_memory(owner, "bed")
 	current_state[StateConditions.KNOWS_DOOR] = .check_agent_memory(owner, "door")
 	current_state[StateConditions.KNOWS_GUN] = .check_agent_memory(owner, "gun")
 	return current_state
-	
-static func any_fires(owner) -> bool:
-	if owner.blackboard.get("fire"):
-		for fire in owner.blackboard.get("fire"):
-			if fire.lit:
-				return true
-	return false
 
-static func any_intruders(owner) -> bool:
-	if owner.blackboard.get("intruder"):
-		for intruder in owner.blackboard.get("intruder"):
-			if intruder.present:
-				return true
-	return false
-
-static func any_doors_knocked(owner) -> bool:
-	if owner.blackboard.get("door"):
-		for door in owner.blackboard.get("door"):
-			if door.knocked:
-				return true
-	return false
-
-static func any_doors_opened(owner) -> bool:
-	if owner.blackboard.get("door"):
-		for door in owner.blackboard.get("door"):
-			if door.opened:
+static func any_of_group(owner, group, field) -> bool:
+	if owner.blackboard.get(group):
+		for object in owner.blackboard.get(group):
+			if object.get(field):
 				return true
 	return false
